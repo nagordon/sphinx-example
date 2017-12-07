f82360fc4dea6aa09f11a0f3f141ab5240c40f78
Neal Gordon <nealagordon@gmail.com>
2017-12-06 20:45:05 -0800

    auto add changes to master branch and updated documentation

--- docs/run_build_html_sphinx.bat
+++ docs/run_build_html_sphinx.bat
@@ -8,7 +8,7 @@ ghp-import build\html -m "updated doc webpage on gh-pages branch" -p
 
 
 git add --all
-git commit -m 'auto add changes to master branch and updated documentation'
+git commit -m "auto add changes to master branch and updated documentation"
 git push --all origin
 
 pause
